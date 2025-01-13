a7e4256593e652c40c837f2d7ab854f611e664c0
Rimarve <rrwvanemmerik@gmail.com>
2025-01-13 21:27:09 +0300

    tussentijds opslaan en Git leren

--- mimo.py
+++ mimo.py
@@ -2,6 +2,9 @@ naam = "Rimar"
 leeftijd = 31
 
 
-print(f"Mijn naam is: " + naam)
+print(f"Mijn naam is: " + naam + f" en mijn leeftijd is: {leeftijd}")
 
-print("test")
+print("Mijn naam is: " + naam + " en mijn leeftijd is: " + str(leeftijd))
+
+print(f"Mijn naam is: {leeftijd}")
+
