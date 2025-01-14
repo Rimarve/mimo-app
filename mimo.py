4e4bab3179318b0250649b4bd0334647ffa6a354
Rimarve <rrwvanemmerik@gmail.com>
2025-01-14 12:58:32 +0300

    Scrimba tutorial string formatting

--- mimo.py
+++ mimo.py
@@ -1,10 +1,14 @@
-naam = "Rimar"
-leeftijd = 31
+# naam = "Rimar"
+# leeftijd = 31
 
 
-print(f"Mijn naam is: " + naam + f" en mijn leeftijd is: {leeftijd}")
+# # print(f"Mijn naam is: " + naam + f" en mijn leeftijd is: {leeftijd}")
+# # print(f"Mijn naam is: {leeftijd}")
 
-print("Mijn naam is: " + naam + " en mijn leeftijd is: " + str(leeftijd))
+# print("Mijn naam is: " + naam + " en mijn leeftijd is: " + str(leeftijd))
+# print(type(2.5))
 
-print(f"Mijn naam is: {leeftijd}")
 
+msg = "Welcome to Python 101: Strings"
+
+print(msg)
