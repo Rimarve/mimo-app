85382696faf80e3704c7427ef6575c0969115f7e
Rimarve <rrwvanemmerik@gmail.com>
2025-01-14 13:16:23 +0300

    Geoefend met strings

--- mimo.py
+++ mimo.py
@@ -8,7 +8,19 @@
 # print("Mijn naam is: " + naam + " en mijn leeftijd is: " + str(leeftijd))
 # print(type(2.5))
 
+# 1 Welcome Ring To Tyler
 
-msg = "Welcome to Python 101: Strings"
+
+# msg = "Welcome to Python 101: Strings"
+# msg1 = msg[18] + " " + msg[0:8] + msg[25:29]
+
+
+# print(msg1.title())
+
+msg = """ 
+Lorum ipsum dollar sit amet
+Dit is een proef tekst
+over meerdere regels in de console
+"""
 
 print(msg)
