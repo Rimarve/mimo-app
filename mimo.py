bcd23a2617529f464938e4c7efcdb20f4ae7da79
Rimarve <rrwvanemmerik@gmail.com>
2025-01-14 17:27:35 +0300

    Stukje van de tutorials String formatting geleerd

--- mimo.py
+++ mimo.py
@@ -1,6 +1,6 @@
-# naam = "Rimar"
-# leeftijd = 31
-
+naam = "Rimar"
+leeftijd = 31
+kleur = "Blauw"
 
 # # print(f"Mijn naam is: " + naam + f" en mijn leeftijd is: {leeftijd}")
 # # print(f"Mijn naam is: {leeftijd}")
@@ -11,16 +11,27 @@
 # 1 Welcome Ring To Tyler
 
 
-# msg = "Welcome to Python 101: Strings"
+msg = "Welcome to Python 101: Strings"
 # msg1 = msg[18] + " " + msg[0:8] + msg[25:29]
 
 
 # print(msg1.title())
 
-msg = """ 
-Lorum ipsum dollar sit amet
-Dit is een proef tekst
-over meerdere regels in de console
-"""
+# msg = """ 
+# Lorum ipsum dollar sit amet
+# Dit is een proef tekst
+# over meerdere regels in de console
+# """
 
-print(msg)
+# print(msg.replace('Lorum','Rimar'))
+
+# msg1 = msg.replace('Rimar','David')
+# print(msg1)
+
+# print('python' in msg)
+# print('Python' in msg)
+# print('python' not in msg)
+
+# 
+msg1 = f"[{naam.capitalize()}] houdt van de kleur {kleur.lower()}"
+print(msg1)
